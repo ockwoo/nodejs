cfbd6f14324088a92361ce579b16c3c2aa2e7686
ockwoo <hyunwoo.ock@gmail.com>
2016-06-04 13:25:17 +0900

    add instgram

---
+++ instagram/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var superagent = require('superagent');
+var consolidate = require('consolidate');
+
+var app = express();
+
+app.engine('html', consolidate.handlebars);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// set up a static folder with middleware
+app.set(express.static(__dirname + '/public'));
+
+var user = 'azat_co';
+var story_slug = 'kazan';
+
+var api_key = "";
+var username = "";
+var _token = "";
+
+app.get('/',function(req, res){
+	superagent.get("http://api.storify.com/v1/stories/" + user + "/" + story_slug)
+	.query({
+		api_key : api_key,
+		username : username,
+		_token : _token
+	})
+	.set({ Accept: 'application/json'})
+	.end(function(e, storifyResponse){
+		if(e) return next(e);
+		return res.render('index', storifyResponse.body.content)
+	});
+});
+
+app.listen(3001);
